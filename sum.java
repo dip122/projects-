db4b9c60f305e3fa5d1dce1a3b5c36821fa7d53d
= <=>
2023-05-28 17:31:54 +0530

    My second commit in git

--- sum.java
+++ sum.java
@@ -2,6 +2,6 @@ class sum{
 	public static void main(){
 		int a = 2;
 		int b = 3;
-		System.out.println("sum of the elements are" + (a+b));
+		System.out.println("Sum of the elements are" + (a+b));
 	}
 }
