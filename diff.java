166bce97c9677a1f96cedfbadc5c3a852541cf89
Abhik Ganguly <abhik.g2002@gmail.com>
2023-05-28 18:28:12 +0530

    Diff.java committed

--- diff.java
+++ diff.java
@@ -2,7 +2,7 @@ class diff{
 	public static void main(){
 		int a = 2;
 		int b = 3;
-		System.out.println("Ki gawd bhai tui 3rd question ta kore dili "+(a-b));
+		System.out.println("Ki gawd bhai tui 3rd question ta kore dili.Ki gawd tui #allgawd "+(a-b));
 
 	}
 }
