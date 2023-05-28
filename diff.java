d0d19fb29ab8a9ac59b5d2fcd1153847dd00ac5e
= <=>
2023-05-28 17:33:15 +0530

    My Third commit

--- diff.java
+++ diff.java
@@ -2,6 +2,6 @@ class diff{
 	public static void main(){
 		int a = 2;
 		int b = 3;
-		System.out.println("difference between two element is "+(a-b));
+		System.out.println("Difference between two element is "+(a-b));
 	}
 }
