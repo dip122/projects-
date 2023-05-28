ead5ebcaa78647b6ca33714a8566270ed2cff268
Abhik Ganguly <abhik.g2002@gmail.com>
2023-05-28 18:19:47 +0530

    Changes committed

--- diff.java
+++ diff.java
@@ -2,6 +2,7 @@ class diff{
 	public static void main(){
 		int a = 2;
 		int b = 3;
-		System.out.println("Difference between two element is "+(a-b));
+		System.out.println("Ki gawd bhai tui 3rd question ta kore dili "+(a-b));
+
 	}
 }
